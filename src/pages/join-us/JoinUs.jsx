8f709cd49a246d5be392b5deba0e5eaae41ac1ca
Fariya <fariya.webdev@gmail.com>
2025-03-14 05:01:41 +0600

    feat: our team section

--- src/pages/join-us/JoinUs.jsx
+++ src/pages/join-us/JoinUs.jsx
@@ -1,6 +1,7 @@
 import { IoIosMail } from "react-icons/io";
 import { MdCall } from "react-icons/md";
 import Socials from "../../components/Shared/Socials";
+import member from "../../Assets/joinus/Team/team.jpg"
 
 const JoinUs = () => {
   return (
@@ -12,21 +13,27 @@ const JoinUs = () => {
 
         {/*TODO: bg picture in this div*/}
         <div className='bg-[#785F54] min-h-96'>
+          <img src="" alt="" />
 
         </div>
+
+
 
         {/* main sections start */}
         <div>
 
+
+
           {/* contact through email section */}
-          <div className='max-w-screen-xl w-[94%] -mt-44 bg-gray-300 mx-auto grid md:grid-cols-2 p-8 rounded-lg '>
+          <div className='max-w-screen-xl w-[94%] -mt-44 bg-gray-300 mx-auto grid md:grid-cols-2 p-6 md:p-8 rounded-lg '>
 
-            <div className='my-auto ml-8'>
+            <div className='my-auto md:ml-8'>
 
-              <h2 className='text-4xl font-bold'>Contact Us To Join</h2>
-              <div className='my-4 text-lg'>
-                <p className='flex items-center gap-2'><MdCall className="text-[#785F54]"/> +01799886655</p>
-                <p className='flex items-center gap-2'><IoIosMail className="text-[#785F54]"/> TickBook@gmail.com</p>
+              <h2 className='text-3xl md:text-4xl font-bold'>Contact Us</h2>
+              <p className="w-[78%] my-3">Want to be part of something exciting? Whether you're looking to join our team, partner with us, or collaborate on events, we'd love to hear from you!</p>
+              <div className='my-4 text-lg flex gap-4'>
+                <p className='flex items-center gap-2'><MdCall className="text-[#785F54]" /> +01799886655</p>
+                <p className='flex items-center gap-1'><IoIosMail className="text-[#785F54]" /> TickBook@gmail.com</p>
               </div>
               <Socials />
 
@@ -34,7 +41,7 @@ const JoinUs = () => {
 
             <div>
 
-              <fieldset className="fieldset space-y-3">
+              <fieldset className="fieldset space-y-3 mt-5 md:mt-0">
 
                 <input type="text" name='name' className="input w-full rounded-md" placeholder="Your Name" />
 
@@ -50,13 +57,82 @@ const JoinUs = () => {
 
           </div>
 
+
+
           {/* map/location section*/}
           <div>
 
           </div>
 
+
+
           {/* team section */}
-          <div>
+          <div className="max-w-screen-xl w-[94%] my-24 mx-auto">
+            <h2 className="text-4xl font-bold">Our Technical Team</h2>
+            <p className="w-[80%] my-5">Behind every great experience is a team of passionate individuals dedicated to making your life easy. At Tickto, our experts in technology, customer support, and event management work together to deliver a seamless platform for you. We believe in innovation, collaboration, and creating unforgettable experiences—one ticket at a time!</p>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+              <div className="rounded-lg">
+
+                <img
+                  className="rounded-lg"
+                  // src={Mushfika} 
+                  src={member}
+                  alt="Mushfika" />
+
+              </div>
+
+              <div className="rounded-lg ">
+
+                <img
+                  className="rounded-lg"
+                  // src={Rakibul} 
+                  src={member}
+                  alt="Rakibul" />
+
+              </div>
+
+              <div>
+
+                <img
+                  className="rounded-lg"
+                  // src={Fariya} 
+                  src={member}
+                  alt="Fariya" />
+
+              </div>
+
+              <div>
+
+                <img
+                  className="rounded-lg"
+                  // src={Minhaj} 
+                  src={member}
+                  alt="Minhaj" />
+
+              </div>
+
+              <div>
+
+                <img
+                  className="rounded-lg"
+                  // src={Nure} 
+                  src={member}
+                  alt="Nure" />
+
+              </div>
+
+              <div>
+
+                <img
+                  className="rounded-lg"
+                  // src={Akash} 
+                  src={member}
+                  alt="Akash" />
+
+              </div>
+
+            </div>
 
           </div>
 
