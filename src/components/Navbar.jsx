de3c21612c1f84e2f3970333311e52bcd3d5f8d7
rakib1514 <rkrakib1514@outlook.com>
2025-03-14 02:17:21 +0600

    Initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <div  className="border border-black py-6">
+      Please create navbar here /src/components/Navbar.jsx
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/join-us/JoinUs.jsx
@@ -0,0 +1,10 @@
+
+const JoinUs = () => {
+  return (
+    <div>
+      Join Us here /src/pages/join-us/JoinUs.jsx
+    </div>
+  );
+};
+
+export default JoinUs;
