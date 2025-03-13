71c322db7b09b4bb0f532cc7caa920491c67385c
Fariya <fariya.webdev@gmail.com>
2025-03-14 03:20:29 +0600

    feat: besic structure of JoinUs page

--- src/pages/join-us/JoinUs.jsx
+++ src/pages/join-us/JoinUs.jsx
@@ -1,8 +1,38 @@
 
 const JoinUs = () => {
   return (
-    <div>
-      Join Us here /src/pages/join-us/JoinUs.jsx
+    <div className="bg-[#fff9f1] min-h-screen">
+
+      {/* </NavBar> */}
+
+      <div>
+
+        {/*TODO: bg picture in this div*/}
+        <div >
+
+        </div>
+
+        {/* main sections start */}
+        <div>
+
+          {/* contact through email section */}
+          <div>
+
+
+          </div>
+
+          {/* map/location section*/}
+          <div>
+
+          </div>
+
+          {/* team section */}
+          <div>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 };
