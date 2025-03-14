d31fbad2538b5400a99ad749aabf7f06e2dab933
unknown <arifk62901@gmail.com>
2025-03-14 15:17:20 +0600

    added to customer sectioin of about us

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -1,3 +1,6 @@
+import About_Executive_team from "./About_Executive_team";
+import About_Join_us_sectioin from "./About_Join_us_section";
+import AboutFromCustomer from "./AboutFromCustomer";
 import AboutUs_first_section from "./AboutUs_first_section";
 import OurTeam from "./OurTeam";
 
@@ -5,10 +8,9 @@ const AboutUs = () => {
   return (
     <div>
       <AboutUs_first_section />
-      {/* <AboutFromCustomer />
-      <OurTeam />
+      <AboutFromCustomer />
       <About_Join_us_sectioin />
-      <About_Executive_team /> */}
+      <About_Executive_team />
       <OurTeam />
     </div>
   );
