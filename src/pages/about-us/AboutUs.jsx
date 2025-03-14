e277db0ee126b3614e909c6582be2bce522c9ea2
unknown <arifk62901@gmail.com>
2025-03-14 15:10:38 +0600

    added to overview of about us

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -1,8 +1,14 @@
+import AboutUs_first_section from "./AboutUs_first_section";
 import OurTeam from "./OurTeam";
 
 const AboutUs = () => {
   return (
     <div>
+      <AboutUs_first_section />
+      {/* <AboutFromCustomer />
+      <OurTeam />
+      <About_Join_us_sectioin />
+      <About_Executive_team /> */}
       <OurTeam />
     </div>
   );
