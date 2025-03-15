679b995aee3daa6eabf0b24e366739c4fa15eb22
Fariya <fariya.webdev@gmail.com>
2025-03-16 03:53:54 +0600

    fix: responsiveness & color

--- src/pages/home/DataSafe.jsx
+++ src/pages/home/DataSafe.jsx
@@ -5,24 +5,24 @@ const DataSafe = () => {
   return (
     <div className='grid md:grid-cols-2 my-32'>
 
-      <div className='bg-gradient-to-r from-[#a2b9a7]/50 to-[#317371]/50 p-32 py-44 '>
+      <div className='bg-gradient-to-r from-[#a2b9a7]/50 to-[#317371]/50 md:p-32 py-20 md:py-44 lg:py-48 '>
         <img className='mx-auto w-56' src={shield} alt="" />
       </div>
 
-      <div className='bg-[#e6e9ed] text-lg p-12 py-28 lg:p-32 lg:py-44'>
+      <div className='bg-[#e6e9ed] text-lg p-12 py-18 lg:p-32 lg:py-44'>
         <h1 className='text-5xl font-bold'>Keeping your data safe</h1>
         <p className='text-lg my-5'>We prioritize your data privacy above all else. Our platform is designed with robust security measures to ensure that your personal information remains safe and protected at all times..</p>
         <ul className='pl-12 space-y-2'>
           <li className='flex gap-2 items-center'>
-            <FaCheck />
+            <FaCheck className="text-[#317371]"/>
             Data security
           </li>
           <li className='flex gap-2 items-center'>
-            <FaCheck />
+            <FaCheck className="text-[#317371]"/>
             Safe and protected
           </li>
           <li className='flex gap-2 items-center'>
-            <FaCheck />
+            <FaCheck className="text-[#317371]"/>
             Transparent handling of your data
           </li>
         </ul>
