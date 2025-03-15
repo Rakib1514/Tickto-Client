2578d11b78bc07172d2e52481f8f7875fb33b59f
Fariya <fariya.webdev@gmail.com>
2025-03-16 03:48:50 +0600

    fix: responsiveness & color

--- src/pages/home/DataSafe.jsx
+++ src/pages/home/DataSafe.jsx
@@ -5,11 +5,11 @@ const DataSafe = () => {
   return (
     <div className='grid md:grid-cols-2 my-32'>
 
-      <div className=' p-32 py-44 '>
+      <div className='bg-gradient-to-r from-[#a2b9a7]/50 to-[#317371]/50 p-32 py-44 '>
         <img className='mx-auto w-56' src={shield} alt="" />
       </div>
 
-      <div>
+      <div className='bg-[#e6e9ed] text-lg p-12 py-28 lg:p-32 lg:py-44'>
         <h1 className='text-5xl font-bold'>Keeping your data safe</h1>
         <p className='text-lg my-5'>We prioritize your data privacy above all else. Our platform is designed with robust security measures to ensure that your personal information remains safe and protected at all times..</p>
         <ul className='pl-12 space-y-2'>
