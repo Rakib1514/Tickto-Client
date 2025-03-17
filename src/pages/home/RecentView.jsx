5029beb1085ef9d79feb54ebed3b606dfb68eda6
Fariya <fariya.webdev@gmail.com>
2025-03-17 07:39:39 +0600

    fix: card data & animation on text

--- src/pages/home/RecentView.jsx
+++ src/pages/home/RecentView.jsx
@@ -12,14 +12,14 @@ const RecentView = () => {
   useEffect(() => {
     fetch('./event.json')
       .then(res => res.json())
-      .then(data => setEvents(data))
+      .then(data => setEvents(data.slice(0, 4)))
   }, [])
 
   console.log(events)
   return (
-    <div className='max-w-screen-2xl w-[96%] mx-auto'>
-      <h1 className='text-3xl font-bold my-10'>Recently Viewed</h1>
-      <div className='grid md:grid-cols-3 lg:grid-cols-4 gap-2'>
+    <div className='max-w-screen-2xl w-[96%] mx-auto my-28'>
+      <h1 className='md:text-4xl font-bold my-14'>Recently Viewed</h1>
+      <div className='grid md:grid-cols-3 lg:grid-cols-4 gap-3'>
         {
           events?.map(event => <SmallCard key={event.id} event={event} />)
         }
