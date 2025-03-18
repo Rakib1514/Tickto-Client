6d07a09cdc15059dfcb1549d429b4e7f9bed6bd0
Fariya <fariya.webdev@gmail.com>
2025-03-18 19:16:05 +0600

    fix: form & reponsiveness

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,50 +1,145 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router';
+import { motion } from 'framer-motion';
+import { FaFacebook, FaGithub, FaGoogle } from 'react-icons/fa';
 
 const Register = () => {
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+    const onSubmit = async (data) => {
+
+        console.log(data)
+
+    };
+
+
     return (
         <div className='min-h-screen flex justify-center items-center'>
-            <div className='blur-bg grid md:grid-cols-2 border border-gray-400 text-white rounded-2xl max-w-screen-xl'>
+            <div className='blur-bg min-h-screen md:min-h-fit grid md:grid-cols-2 border border-gray-400 text-white rounded-2xl max-w-screen-xl md:w-[96%]'>
                 
-                {/* first part */}
-                <div className='p-10 py-20 flex justify-center items-center text-center bg-black/30 rounded-l-2xl'>
+                {/* second part */}
+                <div className='md:hidden md:p-10 py-10 md:py-20 flex justify-center items-center text-center md:bg-black/30 rounded-r-2xl'>
 
                     <div >
                         <h1 className='font-bold text-3xl'>Wellcome to TickTo</h1>
                         <p className='py-6 w-[80%] mx-auto text-sm'>Discover trending events and personalized recommendations. From local gatherings to global spectacles, find experiences that match your interests.Book with confidence using our secure payment system. Enjoy a hassle-free ticketing experience with instant confirmation and digital access.</p>
-                        <p>Do not have an account? <Link to={'/auth/register'}> Register </Link> First</p>
+                        <p>Already have an account? <Link to={'/auth/login'} className='text-[#67B293] link'> Login </Link> Now</p>
 
+                    </div>
+
+                </div>
+
+                {/* first part */}
+                <div className='md:p-6 md:py-20'>
+
+                    <div className=''>
+                        <h3 className='text-2xl text-center font-semibold'>Sign In</h3>
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+
+                            {/* name */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text py-1">Name</span>
+                                </label>
+                                <br />
+
+                                <input
+                                    type="text"
+                                    placeholder="Your Name"
+                                    {...register("user_name")}
+                                    className="input input-bordered w-full bg-gray-200 text-gray-900" required />
+
+                            </div>
+                            
+                            {/* email */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text py-1">Email</span>
+                                </label>
+                                <br />
+
+                                <input
+                                    type="email"
+                                    placeholder="Your Email"
+                                    {...register("user_email")}
+                                    className="input input-bordered w-full bg-gray-200 text-gray-900" required />
+
+                            </div>
+                            
+                            {/* photo */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text py-1">Photo</span>
+                                </label>
+                                <br />
+
+                                <input
+                                    type="file"
+                                    placeholder="Your Email"
+                                    {...register("user_image")}
+                                    className='file-input file-input-ghost file-input-bordered w-full bg-gray-200 text-gray-500 mx-auto' required />
+
+                            </div>
+
+                            {/* email */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text p-1">Password</span>
+                                </label>
+                                <br />
+
+                                <input
+                                    type="password"
+                                    placeholder="Your Password"
+                                    {...register("password")}
+                                    className="input input-bordered w-full bg-gray-200 text-gray-900" required />
+
+                            </div>
+
+
+                            <div className="form-control mt-6">
+                                <motion.button
+                                    whileHover={{ scale: 1.02 }}
+                                    whileTap={{ scale: 0.95 }}
+                                    className="btn border-none bg-[#A989B0] text-white shadow-none w-full">Register</motion.button>
+                            </div>
+                        </form>
+
+                        <div className=' w-1/3 mx-auto mt-5 flex gap-3 justify-center'>
+                            <div className='border border-[#67B293] rounded-full p-1 '>
+                                <FaFacebook className='text-xl text-[#A989B0] hover:text-[#67B293]' />
+                            </div>
+
+                            <div className='border border-[#67B293] rounded-full p-1 '>
+                                <FaGoogle className='text-xl text-[#A989B0] hover:text-[#67B293]' />
+                            </div>
+
+                            <div className='border border-[#67B293] rounded-full p-1'>
+                                <FaGithub className='text-xl text-[#A989B0] hover:text-[#67B293]' />
+                            </div>
+                        </div>
                     </div>
 
                 </div>
 
                 {/* second part */}
-                <div className='p-6 py-20'>
+                <div className='hidden md:block '>
+                
+                <div className='md:p-10 py-10 h-full md:py-20 flex justify-center items-center text-center md:bg-black/30 rounded-r-2xl'>
 
-                    <div className=''>
-                        <h3 className='text-2xl text-center font-semibold'>Login here</h3>
-                        <form className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label> <br />
-                                <input type="email" placeholder="email" className="input input-bordered w-full bg-gray-200 text-gray-900" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label> <br />
-                                <input type="password" placeholder="password" className="input input-bordered w-full bg-gray-200 text-gray-900" required /> <br />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn border-none bg-[#562a83] shadow-none w-full">Login</button>
-                            </div>
-                        </form>
+                    <div >
+                        <h1 className='font-bold text-3xl'>Wellcome to TickTo</h1>
+                        <p className='py-6 w-[80%] mx-auto text-sm'>Discover trending events and personalized recommendations. From local gatherings to global spectacles, find experiences that match your interests.Book with confidence using our secure payment system. Enjoy a hassle-free ticketing experience with instant confirmation and digital access.</p>
+                        <p>Already have an account? <Link to={'/auth/login'} className='text-[#67B293] link'> Login </Link> Now</p>
+
                     </div>
 
                 </div>
+
+                </div>
+
             </div>
         </div>
     );
