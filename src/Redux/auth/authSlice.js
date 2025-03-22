6e9b1677c4d3bdb13fbfa08fe662050a2f93a078
Fariya <fariya.webdev@gmail.com>
2025-03-22 06:33:13 +0600

    feat: redux store & auth slice

--- src/Redux/auth/authSlice.js
+++ src/Redux/auth/authSlice.js
@@ -1,14 +1,85 @@
-import { createSlice } from "@reduxjs/toolkit";
 
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import auth from '../../firebase/firebase.init';
+
+// ALL THE THUNKS HERE
+
+// register user
+export const registerUser = createAsyncThunk('auth/register', async ({ email, password }, { rejectWithValue }) => {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        return userCredential.user;
+    }
+    catch (err) {
+        return rejectWithValue(err)
+    }
+})
+
+// login user
+export const loginUser = createAsyncThunk('auth/login', async ({ email, password }, { rejectWithValue }) => {
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        return userCredential.user;
+    }
+    catch (err) {
+        return rejectWithValue(err)
+    }
+})
+
+// Logout User
+export const logoutUser = createAsyncThunk("auth/logout", async (_, { rejectWithValue }) => {
+    try {
+        await signOut(auth);
+        return null;
+    }
+    catch (error) {
+        return rejectWithValue(error);
+    }
+});
 
 const authSlice = createSlice({
     name: 'auth',
-    initial: {
+    initialState: {
         user: null,
-        loading: false, 
+        loading: false,
         error: null
     },
-    reducers: {
-        
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(registerUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload;
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+
+            .addCase(loginUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload;
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+
+            .addCase(logoutUser.fulfilled, (state) => {
+                state.user = null;
+            });
     }
-})
+});
+
+// export const { } = authSlice.actions
+
+export default authSlice.reducer
