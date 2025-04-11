b83d208e774bb1a483bf6ad9bc8953b9e42996ab
Fariya <fariya.webdev@gmail.com>
2025-04-11 17:07:58 +0600

    feat: new banner & navbar

--- src/components/Shared/Logo.jsx
+++ src/components/Shared/Logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from '../../Assets/icons/ticket.png'
+import logo2 from '../../Assets/icons/fly.png'
+import logo3 from '../../Assets/icons/plane-ticket.png'
 
 const Logo = () => {
     return (
         <div>
-           <img src={logo} className='w-10 md:w-12'  alt="logo" /> 
+           <img src={logo2} className='w-10 md:w-12'  alt="logo" /> 
         </div>
     );
 };
