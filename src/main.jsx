7b05de2cff10c78281b9edd557cfe9ea46bd4bce
musfikanoni <musfikanoni@gmail.com>
2025-04-19 23:59:51 +0600

    updates routes data mood

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, RouterProvider } from 'react-router';
 import PublicRoutes from './routes/PublicRoutes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import axios from 'axios';
@@ -21,7 +21,8 @@ createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <AuthProvider>
           <QueryClientProvider client={queryClient}>
-            <PublicRoutes />
+            <RouterProvider router={} />
+            {/* <PublicRoutes /> */}
           </QueryClientProvider>
         </AuthProvider>
       </Provider>
